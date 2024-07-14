e5cfecd41984f39e9971609bbe8630fdc4aeb470
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-07-14 21:50:58 +0600

    custom middlewar

--- redux-basic/src/redux/store.js
+++ redux-basic/src/redux/store.js
@@ -9,6 +9,10 @@ const myLogger = (store) => (next) => (action) => {
     console.log(`Action:${JSON.stringify(action)}`);
     console.log(`Before state: ${JSON.stringify(store.getState())}`);
 
+    const upcomingState = [action].reduce(rootReducer, store.getState());
+
+    console.log(`Upcoming state: ${JSON.stringify(upcomingState)}`);
+
     // pass action
 
     return next(action);
