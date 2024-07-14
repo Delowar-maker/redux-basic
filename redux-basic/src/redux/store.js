827ecf3a1f0530b02f60a937e777d0da51fecd48
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-07-14 21:43:27 +0600

    curry function and using middlewar

--- redux-basic/src/redux/store.js
+++ redux-basic/src/redux/store.js
@@ -1,6 +1,18 @@
 
-import { legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer);
+
+// create aor first middleware using curry function 
+
+const myLogger = (store) => (next) => (action) => {
+    console.log(`Action:${JSON.stringify(action)}`);
+    console.log(`Before state: ${JSON.stringify(store.getState())}`);
+
+    // pass action
+
+    return next(action);
+
+}
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 export default store
