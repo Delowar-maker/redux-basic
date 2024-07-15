eed0afcf78b21b29381588e566ed839a28f48e6c
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-07-16 01:50:44 +0600

    reducer todos

---
+++ todo-slice/src/redux/todos/initialState.js
@@ -0,0 +1,14 @@
+export const initialState = [
+    {
+        id: 1,
+        text: 'Learn React js',
+        completed: true,
+    },
+    {
+        id: 1,
+        text: 'Learn Redux',
+        completed: false,
+        color: "red",
+
+    },
+]
