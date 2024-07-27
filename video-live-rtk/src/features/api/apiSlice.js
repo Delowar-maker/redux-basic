a88a229fa06a38e8b8a5c6ae68abbd376293ae4a
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-07-27 17:17:25 +0600

    api query complete same all application

--- video-live-rtk/src/features/api/apiSlice.js
+++ video-live-rtk/src/features/api/apiSlice.js
@@ -13,8 +13,16 @@ export const apiSlice = createApi({
         getVideo: builder.query({
             query: (VideoId) => `/videos/${VideoId}`,
         }),
+        // ?title_like=css&tile_like=lailwind&_limit=4 (build up query string)
+
+        query: ({ id, title }) => {
+            const tags = title.split(" "); // faka array paop
+            const likes = tags.map((tag) => `title_like=${tag}`);
+            const queryString = `/videos?${likes.join("&")}&_limit=4`;
+            return queryString;
+        },
 
     }),
 });
 
-export const { useGetVideosQuery, useGetVideoQuery } = apiSlice;
+export const { useGetVideosQuery, useGetVideoQuery, useGetRelatedVideosQuery } = apiSlice;
